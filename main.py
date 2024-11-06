ed96fb941eb6ffb36394a021340975b4855217ee
Mathyou <142758570+diabau@users.noreply.github.com>
2024-11-06 22:49:33 +0100

    Update weather API request and temperature display
    This commit updates the weather API request in the main.py file. It replaces the hardcoded API key, latitude, longitude, and altitude values with a dictionary of parameters. Additionally, the temperature display is rounded to two decimal places. Finally, a new request is made to a webhook with the current temperature in Saint-Étienne.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
+params = {
+    "apikey": "APIKEY",
+    "lat": 45.43,
+    "lon": 4.3872,
+    "asl": 480, # Altitude
+    "format": "json"
+}
 
-url = "https://my.meteoblue.com/packages/current?apikey=(API_KEY)&lat=45.43&lon=4.4&asl=479&format=json"
 
-response = requests.get(url)
-
+response = requests.get("https://my.meteoblue.com/packages/current", params=params)
 temperture = response.json()['data_current']['temperature']
 
-print(temperture)
+temperture = round(temperture, 2)
+requete = requests.post("WEBHOOK", json={"username": "meteo", "content": "La température actuelle à Saint-Étienne est d'environ " + str(temperture) + "°C"})
